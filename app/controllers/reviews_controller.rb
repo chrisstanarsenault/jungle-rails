0b6b44ef82891f9f45cbe5cc8b185f1297bd999c
Chris <chrisstanarsenault@gmail.com>
2018-12-07 17:12:57 -0500

    added logic for CRUD to show ratings and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
 
 end
